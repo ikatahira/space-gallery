92db67a77115d78aa4faa81642f9b9a79d7a54a1
ikatahira <69829022+ikatahira@users.noreply.github.com>
2026-05-11 02:11:38 +0000

    Corrigindo herança de templates e adicionando fotos

--- galeria/views.py
+++ galeria/views.py
@@ -3,19 +3,9 @@ from django.shortcuts import render, get_object_or_404
 from galeria.models import Fotografia
 
 def index(request):
-    fotografias = Fotografia.objects
-        .order_by("data_fotografia")
-        .filter(publicada=True)
-    return render(
-        request, 
-        'galeria/index.html', 
-        {"cards": fotografias}
-    )
+    fotografias = Fotografia.objects.order_by("data_fotografia").filter(publicada=True)
+    return render(request,'galeria/index.html',{"cards": fotografias})
 
 def imagem(request, foto_id):
     fotografia = get_object_or_404(Fotografia, pk=foto_id)
-    return render(
-        request, 
-        'galeria/imagem.html', 
-        {"fotografia": fotografia}
-    )
+    return render(request,'galeria/imagem.html', {"fotografia": fotografia})
