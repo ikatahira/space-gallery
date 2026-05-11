e4d53ddc3f10c90f71cc1f3498dc22f627396536
ikatahira <69829022+ikatahira@users.noreply.github.com>
2026-05-11 00:42:32 +0000

    Aula 02: app galeria criada

--- setup/settings.py
+++ setup/settings.py
@@ -47,6 +47,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "galeria",   # ← adicione esta linha
+
 ]
 
 MIDDLEWARE = [
@@ -64,7 +66,7 @@ ROOT_URLCONF = "setup.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [os.path.join(BASE_DIR, 'templates')],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
